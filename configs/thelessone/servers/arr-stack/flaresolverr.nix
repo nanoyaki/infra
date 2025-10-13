18b2c7ae27b6c6087a4d387f3dd3c88dd184fe69
nanoyaki <hanakretzer@nanoyaki.space>
2025-10-13 19:16:55 +0200

    fix: remove flaresolverr xvfbwrapper override

--- configs/thelessone/servers/arr-stack/flaresolverr.nix
+++ configs/thelessone/servers/arr-stack/flaresolverr.nix
@@ -7,21 +7,4 @@
     enable = true;
     port = 8191;
   };
-
-  nixpkgs.overlays = [
-    (_: prev: {
-      pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
-        (pyFinal: pyPrev: {
-          xvfbwrapper = pyPrev.xvfbwrapper.overridePythonAttrs {
-            format = null;
-            pyproject = true;
-
-            build-system = [ pyFinal.setuptools ];
-
-            doCheck = true;
-          };
-        })
-      ];
-    })
-  ];
 }
