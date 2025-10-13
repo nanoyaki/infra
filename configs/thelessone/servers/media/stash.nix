1249a92475bac1e41784b4300ea9b3d109f438fa
nanoyaki <hanakretzer@nanoyaki.space>
2025-10-13 21:23:18 +0200

    fix: use chromium instead of ungoogled chromium

--- configs/thelessone/servers/media/stash.nix
+++ configs/thelessone/servers/media/stash.nix
@@ -170,7 +170,7 @@ in
         ];
 
         scraper_user_agent = "Mozilla/5.0 (X11; Linux x86_64; rv:139.0) Gecko/20100101 Firefox/139.0";
-        scraper_cdp_path = lib.getExe pkgs.ungoogled-chromium;
+        scraper_cdp_path = lib.getExe pkgs.chromium;
 
         ui = {
           advancedMode = true;
