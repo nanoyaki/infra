a3750dbd9f51ad495003aa65affd45a1c8927cb4
nanoyaki <hanakretzer@nanoyaki.space>
2025-11-03 19:02:06 +0100

    feat(thelessone): beyond-depth backup

--- configs/thelessone/servers/minecraft/backups.nix
+++ configs/thelessone/servers/minecraft/backups.nix
@@ -4,19 +4,30 @@
 }:
 
 {
-  sops.secrets."restic/smp" = { };
+  sops.secrets = {
+    "restic/smp" = { };
+    "restic/beyond-depth" = { };
+  };
 
   config'.restic.backups.smp = {
     repository = "/mnt/raid/backups/smp";
     passwordFile = config.sops.secrets."restic/smp".path;
 
-    basePath = config.services.minecraft-servers.dataDir;
-    paths = [ "smp/world" ];
+    basePath = "${config.services.minecraft-servers.dataDir}/smp/world";
     exclude = [
-      "smp/world/**/data/DistantHorizons*"
-      "smp/world/datapacks"
-      "smp/world/**/*.bak"
+      "**/data/DistantHorizons*"
+      "datapacks"
+      "**/*.bak"
     ];
+
+    timerConfig.OnCalendar = "*:0/30";
+  };
+
+  config'.restic.backups.beyond-depth = {
+    repository = "/mnt/raid/backups/beyond-depth";
+    passwordFile = config.sops.secrets."restic/beyond-depth".path;
+
+    basePath = "/home/thelessone/Dokumente/MinecraftServers/Niklas/niklas3";
 
     timerConfig.OnCalendar = "*:0/30";
   };
