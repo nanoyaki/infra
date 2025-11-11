f121434a8a3351057e9dc8a1ccb627aef1864f41
nanoyaki <hanakretzer@nanoyaki.space>
2025-11-11 23:08:58 +0100

    fix: p1 <-> p2

--- configs/thelessone/hardware/mounts.nix
+++ configs/thelessone/hardware/mounts.nix
@@ -12,17 +12,17 @@
     ];
   };
 
-  fileSystems."/" = {
-    device = "/dev/nvme0n1p1";
-    fsType = "btrfs";
-  };
-
   fileSystems."/boot" = {
-    device = "/dev/nvme0n1p2";
+    device = "/dev/nvme0n1p1";
     fsType = "vfat";
     options = [
       "fmask=0077"
       "dmask=0077"
     ];
+  };
+
+  fileSystems."/" = {
+    device = "/dev/nvme0n1p2";
+    fsType = "btrfs";
   };
 }
