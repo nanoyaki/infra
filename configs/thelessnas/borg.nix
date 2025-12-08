83c2161e7972da661a7969e6e439157a848ca668
nanoyaki <hanakretzer@nanoyaki.space>
2025-12-08 06:44:47 +0100

    feat(thelessnas): add hass repo

--- configs/thelessnas/borg.nix
+++ configs/thelessnas/borg.nix
@@ -1,8 +1,17 @@
 {
-  services.borgbackup.repos.postgres = {
-    path = "/moon/borgbackup/postgres";
-    authorizedKeys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBTF3kpnHtw/4kr1/bLcIer3+WDnO9TQnAIskg0paP0q"
-    ];
+  services.borgbackup.repos = {
+    postgres = {
+      path = "/moon/borgbackup/postgres";
+      authorizedKeys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBTF3kpnHtw/4kr1/bLcIer3+WDnO9TQnAIskg0paP0q"
+      ];
+    };
+
+    hass = {
+      path = "/moon/borgbackup/hass";
+      authorizedKeys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBm2A8lsONm20BXepVyA/Aot6SwqZ4cuwX+tPQQgPmde"
+      ];
+    };
   };
 }
