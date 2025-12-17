625abe8dc67282faa60c832ddf2cfe4eee36ee8f
nanoyaki <hanakretzer@nanoyaki.space>
2025-12-17 09:41:22 +0100

    chore: declutter minecraft directory

--- configs/thelessone/servers/minecraft/declarative-gamerules.nix
+++ configs/thelessone/servers/minecraft/declarative-gamerules.nix
@@ -1,6 +1,7 @@
 {
   lib,
   stdenvNoCC,
+  writeText,
   gamerules ? { },
 }:
 
@@ -10,34 +11,41 @@ let
     map
     attrNames
     isBool
+    mapAttrs
     ;
 
   toValidString =
     actual: if isBool actual then (if actual then "true" else "false") else toString actual;
   toGamerule = gamerule: "gamerule ${gamerule} ${toValidString gamerules.${gamerule}}";
-  renderedGamerules = concatStringsSep "\n" (map toGamerule (attrNames gamerules));
+  renderedGamerules = writeText "setup.mcfunction" (
+    concatStringsSep "\n" (map toGamerule (attrNames gamerules))
+  );
+
+  jsonFiles = mapAttrs (name: json: writeText name (builtins.toJSON json)) {
+    "load.json".values = [ "declarative_gamerules:setup" ];
+    "pack.mcmeta".pack = {
+      description = "Sets gamerules declaratively";
+      pack_format = 81;
+      supported_formats = [
+        81
+        81
+      ];
+    };
+  };
 in
 
 stdenvNoCC.mkDerivation {
   pname = "declarative-gamerules";
   version = "1.0.0";
 
-  src = ./declarative-gamerules;
-
-  buildPhase = ''
-    runHook preBuild
-
-    mkdir -p data/declarative_gamerules/function
-    echo '${renderedGamerules}' > data/declarative_gamerules/function/setup.mcfunction
-
-    runHook postBuild
-  '';
-
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out
-    cp -r * $out
+    mkdir -p $out/data/{minecraft/tags/function,declarative_gamerules/function}
+    ln -s ${jsonFiles."pack.mcmeta"} > $out/pack.mcmeta
+    ln -s ${jsonFiles."load.json"} > $out/data/minecraft/tags/function/load.json
+    ln -s ${renderedGamerules} $out/data/declarative_gamerules/function/setup.mcfunction
+    ln -s ${./icon.png} $out/pack.png
 
     runHook postInstall
   '';
