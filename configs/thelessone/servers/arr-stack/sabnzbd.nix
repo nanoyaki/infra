c9184c34aaee9cf7ceb82d832ca7c63f8e2c2cbb
nanoyaki <hanakretzer@nanoyaki.space>
2026-01-16 10:20:53 +0100

    fix(thelessone/sabnzbd): restore settings from backup

--- configs/thelessone/servers/arr-stack/sabnzbd.nix
+++ configs/thelessone/servers/arr-stack/sabnzbd.nix
@@ -1,21 +1,139 @@
-{ config, ... }:
+{ lib, config, ... }:
 
 let
   domain = "sabnzbd.theless.one";
 in
 
 {
+  sops.secrets = {
+    "sabnzbd/api-key" = { };
+    "sabnzbd/nzb-key" = { };
+
+    "sabnzbd/username" = { };
+    "sabnzbd/password" = { };
+
+    "sabnzbd/eweka-username" = { };
+    "sabnzbd/eweka-password" = { };
+  };
+
+  sops.templates."secrets.ini".content = ''
+    [misc]
+    username = ${config.sops.placeholder."sabnzbd/username"}
+    password = ${config.sops.placeholder."sabnzbd/password"}
+
+    api_key = ${config.sops.placeholder."sabnzbd/api-key"}
+    nzb_key = ${config.sops.placeholder."sabnzbd/nzb-key"}
+
+    [servers]
+    [[news.eweka.nl]]
+    username = ${config.sops.placeholder."sabnzbd/eweka-username"}
+    password = ${config.sops.placeholder."sabnzbd/eweka-password"}
+  '';
+
   services.vopono.allowedTCPPorts = [ 8080 ];
 
   systemd.services.sabnzbd.unitConfig.RequiresMountsFor = "/mnt/raid";
   services.sabnzbd = {
     enable = true;
     inherit (config.arr) group;
-    settings.misc.host_whitelist = "sabnzbd.theless.one";
+
+    secretFiles = [ config.sops.templates."secrets.ini".path ];
+
+    settings = {
+      misc = {
+        # Webinterface
+        web_color = "Night";
+        refresh_rate = 1;
+
+        # Proxy
+        host = "127.0.0.1";
+        inet_exposure = 4;
+        port = 8080;
+        host_whitelist = [ "sabnzbd.theless.one" ];
+        verify_xff_header = 1;
+        local_ranges = [
+          # LAN
+          "10.0.0.0/24"
+          # VPN
+          "100.64.64.0/24"
+        ];
+
+        # Settings where i don't
+        # know what they do
+        sorters_converted = 1;
+        direct_unpack_tested = 1;
+
+        # Files
+        permissions = 2770;
+        download_dir = "${config.arr.home}/downloads/incomplete";
+        complete_dir = "${config.arr.home}/downloads/complete";
+        no_dupes = 4;
+        direct_unpack = 1;
+        history_retention_number = 1;
+        check_new_rel = 1;
+
+        # Keep the disk usable
+        complete_free = "100G";
+        fulldisk_autoresume = 1;
+      };
+
+      logging.log_level = 2;
+
+      servers."news.eweka.nl" = {
+        enable = true;
+        required = true;
+
+        displayname = "Eweka";
+        name = "news.eweka.nl";
+        host = "news.eweka.nl";
+        port = 563;
+        timeout = 60;
+        connections = 50;
+        ssl = 1;
+        ssl_verify = 3;
+
+        expire_date = "2026-06-06";
+      };
+
+      categories =
+        lib.mapAttrs
+          (
+            name: overrides:
+            {
+              inherit name;
+              pp = "";
+              script = "Default";
+              dir = "";
+              newzbin = "";
+              priority = -100;
+            }
+            // overrides
+          )
+          {
+            "*" = {
+              order = 0;
+              pp = 3;
+              script = "None";
+              priority = 0;
+            };
+
+            sonarr.order = 1;
+            radarr.order = 2;
+            prowlarr.order = 3;
+            direct-prowlarr.order = 4;
+            whisparr.order = 5;
+
+            shoko.order = 6;
+            shoko.dir = "${config.arr.home}/downloads/shoko";
+
+            beets.order = 7;
+            beets.dir = "${config.arr.home}/downloads/beets";
+          };
+    };
   };
 
   config'.caddy.vHost.${domain} = {
-    proxy.port = 8080;
+    proxy = { inherit (config.services.sabnzbd.settings.misc) port; };
     useVpn = true;
   };
 
