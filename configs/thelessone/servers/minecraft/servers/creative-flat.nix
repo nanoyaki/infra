3184e62fe23601d8ac9ab09ae2a30747e070bd6b
nanoyaki <hanakretzer@nanoyaki.space>
2026-01-17 21:51:58 +0100

    fix(minecraft): infinite recursion

--- configs/thelessone/servers/minecraft/servers/creative-flat.nix
+++ configs/thelessone/servers/minecraft/servers/creative-flat.nix
@@ -1,37 +1,103 @@
-{ lib, config, ... }:
+{ pkgs, ... }:
 
 {
-  services.minecraft-servers'.servers.creative-flat =
-    lib.recursiveUpdate config.services.minecraft-servers'.servers.smp-creative
-      {
-        enable = true;
+  services.minecraft-servers'.servers.creative-flat = {
+    enable = true;
+    package = pkgs.fabricServers.fabric-1_21_11;
+    jvmOpts = "-Xms8G -Xmx8G";
 
-        serverProperties = {
-          server-port = 30054;
+    serverProperties = {
+      server-port = 30054;
 
-          level-type = "minecraft\\:flat";
-          generator-settings = builtins.toJSON {
-            biome = "minecraft:plains";
-            layers = [
-              {
-                block = "minecraft:bedrock";
-                height = 1;
-              }
-              {
-                block = "minecraft:dirt";
-                height = 2;
-              }
-              {
-                block = "minecraft:grass_block";
-                height = 1;
-              }
-            ];
-          };
-        };
+      gamemode = "creative";
+      difficulty = "normal";
+      level-seed = "-7952476580899652458";
 
-        symlinks."config/voicechat/voicechat-server.properties".value = {
-          port = 24456;
-          voice_host = "theless.one:24456";
-        };
+      level-type = "minecraft\\:flat";
+      generator-settings = builtins.toJSON {
+        biome = "minecraft:plains";
+        layers = [
+          {
+            block = "minecraft:bedrock";
+            height = 1;
+          }
+          {
+            block = "minecraft:dirt";
+            height = 2;
+          }
+          {
+            block = "minecraft:grass_block";
+            height = 1;
+          }
+        ];
       };
+    };
+
+    # TODO: use a proper permission system
+    operators = import ./whitelist.nix;
+
+    gamerules = {
+      # Creative specific
+      keep_inventory = true;
+      spawn_mobs = false;
+      mob_griefing = false;
+
+      # Disable movement checks due
+      # to server upload speed limits
+      elytra_movement_check = false;
+      player_movement_check = false;
+
+      # Minecart speed
+      "ace:speed_player" = 64;
+    };
+
+    mods = map (mod: mod.latest) (
+      with pkgs.minecraft.fabric.v1_21_11;
+      [
+        fabric-api
+        fabricproxy-lite
+        simple-voice-chat
+        vmp-fabric
+        lithium
+        player-roles
+        no-chat-reports
+        krypton
+        c2me-fabric
+        image2map
+        netherportalfix
+        balm
+        ferrite-core
+        scalablelux
+        do-a-barrel-roll
+        cicada
+        servux
+        architectury-api
+        cloth-config
+        yacl
+        audaki-cart-engine
+
+        axiom
+        carpet
+
+        # use jei as long as rei isn't
+        # supported for mc 1.21.11
+        # rei
+        jei
+      ]
+    );
+
+    datapacks = map (datapack: datapack.latest) (
+      with pkgs.minecraft.datapack.v1_21_11;
+      [
+        dungeons-and-taverns
+        # joshs-more-foods
+        mini-blocks-datapack
+      ]
+    );
+
+    symlinks."config/voicechat/voicechat-server.properties".value = {
+      port = 24456;
+      voice_host = "theless.one:24456";
+    };
+  };
 }
