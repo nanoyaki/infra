56b0d12b5cff13e259149ac9fd5f10fe0018b306
nanoyaki <hanakretzer@nanoyaki.space>
2026-01-20 22:00:57 +0100

    fix(sentinel): remove silly comments

--- configs/sentinel/networking.nix
+++ configs/sentinel/networking.nix
@@ -5,7 +5,7 @@
 
   networking.wireguard.interfaces.wg0 = {
     ips = [
-      "100.64.64.23/32" # CGNAT?
+      "100.64.64.23/32"
       "fd64::23/128"
     ];
     privateKeyFile = config.sops.secrets.wg0.path;
@@ -15,7 +15,7 @@
         publicKey = "JB0jviICHpiTm1PYjm4+FCWCPLAjU/NZBm6tRO6/XGY=";
         endpoint = "at01.theless.one:51820";
         allowedIPs = [
-          "100.64.64.1/32" # CGNAT?
+          "100.64.64.1/32"
           "fd64::1/128"
         ];
         persistentKeepalive = 25;
