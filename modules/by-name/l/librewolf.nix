b975f02b1ce16175812d6d18b361c4aab9b7259c
nanoyaki <hanakretzer@nanoyaki.space>
2026-01-23 19:58:31 +0100

    fix(modules/librewolf): use stable librewolf

--- modules/by-name/l/librewolf.nix
+++ modules/by-name/l/librewolf.nix
@@ -28,7 +28,7 @@ in
   options.config'.librewolf.enable = mkFalseOption;
 
   config = mkIf config.config'.librewolf.enable {
-    programs.firefox.package = pkgs.librewolf;
+    programs.firefox.package = pkgs.stable.librewolf;
     environment.sessionVariables.BROWSER = config.programs.firefox.package.meta.mainProgram;
 
     xdg.mime = { inherit defaultApplications; };
