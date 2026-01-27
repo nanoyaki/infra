a8d5d99b85fe8cf8a9a47225cd275acf84514718
nanoyaki <hanakretzer@nanoyaki.space>
2026-01-27 17:29:32 +0100

    chore(minecraft): ban Polypsycism

--- configs/thelessone/servers/minecraft/servers/banned.nix
+++ configs/thelessone/servers/minecraft/servers/banned.nix
@@ -1,3 +1,4 @@
 {
   NoWAY5 = "9760028f-eaeb-4699-8a46-a204f5b1feac";
+  Polypsycism = "f6f31813-9e2c-48b1-9f44-b02e1d5df0cd";
 }
--- configs/thelessone/servers/minecraft/servers/whitelist.nix
+++ configs/thelessone/servers/minecraft/servers/whitelist.nix
@@ -25,7 +25,6 @@
   vampriyva = "adddeb49-1c25-444d-bfa9-f06d94ee057f";
   TwistOfKills = "fe9c7733-6675-45c8-9555-b2993ee9a44c";
   LostYuu = "9739260b-a936-4c37-a6ea-2c10789d2ed8";
-  Polypsycism = "f6f31813-9e2c-48b1-9f44-b02e1d5df0cd";
   Ami951 = "649b6235-f83f-424f-b22a-284ec2e0fd8e";
   SleeperLulu = "bcd341a1-8bfc-498b-a1e7-9fdd06e28860";
   Daxbau = "18695a71-b3fe-4e7d-860e-d84367486b6d";
