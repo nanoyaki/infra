0e0c4ef2fee418716de8418577f97aa78e33fea7
nanoyaki <hanakretzer@nanoyaki.space>
2026-02-03 02:42:12 +0100

    fix(dashboard): symlinkJoin -> overrideAttrs

--- nix/configs/thelessone/servers/enduser/dashboard.nix
+++ nix/configs/thelessone/servers/enduser/dashboard.nix
@@ -555,10 +555,8 @@
       {
         inherit dashboard-wallpaper;
 
-        homepage-dashboard = final.symlinkJoin {
-          inherit (prev.homepage-dashboard) meta pname version;
-          paths = [ prev.homepage-dashboard ];
-          postBuild = ''
+        homepage-dashboard = prev.homepage-dashboard.overrideAttrs {
+          postInstall = ''
             mkdir -p $out/share/homepage/public/images
             # Reduce image size to about 100kb
             ${final.lib.getExe' final.imagemagick "magick"} ${dashboard-wallpaper} \
