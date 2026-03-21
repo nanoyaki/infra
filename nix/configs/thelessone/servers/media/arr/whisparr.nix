9ba03a7b2501b762822eca0adea3021cdabb7b4a
nanoyaki <hanakretzer@nanoyaki.space>
2026-03-21 23:43:52 +0100

    fix(thelessone): deactivate whisparr

--- nix/configs/thelessone/servers/media/arr/whisparr.nix
+++ nix/configs/thelessone/servers/media/arr/whisparr.nix
@@ -5,18 +5,20 @@
     {
       services.vopono.allowedTCPPorts = [ config.services.whisparr.settings.server.port ];
 
-      systemd.services.whisparr.unitConfig.RequiresMountsFor = "/mnt/raid";
+      # systemd.services.whisparr.unitConfig.RequiresMountsFor = "/mnt/raid";
       services.whisparr = {
-        enable = true;
+        enable = false;
         inherit (config.thelessone.arr) group;
       };
 
       thelessone.caddy.vHost."whisparr.theless.one" = {
         proxy.port = config.services.whisparr.settings.server.port;
         useVpn = true;
+        enable = false;
       };
 
       services.borgbackup.jobs.whisparr = {
+        enable = false;
         repo = "thelessone-borg@10.0.0.6:whisparr";
         environment.BORG_RSH = "ssh -i ${config.sops.secrets.id_borg_thelessone.path}";
         doInit = true;
