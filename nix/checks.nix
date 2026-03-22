14c77a441933f5f8762d4508dc79709c7e6f642b
nanoyaki <hanakretzer@nanoyaki.space>
2026-03-22 18:56:32 +0100

    feat(ci): add checks for packages and nixos configs

--- nix/checks.nix
+++ nix/checks.nix
@@ -2,11 +2,18 @@
 
 {
   perSystem =
-    { lib, system, ... }:
+    {
+      lib,
+      pkgs,
+      system,
+      config,
+      ...
+    }:
 
     {
       checks =
-        lib.mapAttrs'
+        # Home configurations
+        (lib.mapAttrs'
           (name: cfg: {
             name = "home-configuration-${name}";
             value = cfg.activationPackage;
@@ -15,6 +22,25 @@
             lib.filterAttrs (
               _: cfg: cfg.pkgs.stdenv.hostPlatform.system == system
             ) inputs.self.homeConfigurations
-          );
+          )
+        )
+        # NixOS configurations
+        // (lib.mapAttrs'
+          (name: cfg: {
+            name = "nixos-configuration-${name}";
+            value = cfg.system.build.toplevel;
+          })
+          (
+            lib.filterAttrs (
+              _: cfg: cfg.config.nixpkgs.hostPlatform.system == system
+            ) inputs.self.nixosConfigurations
+          )
+        )
+        # Packages
+        // (lib.mapAttrs' (name: lib.nameValuePair "package-${name}") (
+          lib.filterAttrs (
+            _: pkg: !(pkg.meta.broken or false) && lib.meta.availableOn pkgs.stdenv.hostPlatform pkg
+          ) config.packages
+        ));
     };
 }
