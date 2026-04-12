25be48447df7ad04b5d639bb8232bd2ff214c4b5
nanoyaki <hanakretzer@nanoyaki.space>
2026-04-13 01:11:31 +0200

    fix(thelessone): disable ipv6

--- nix/configs/thelessone/networking/pangolin.nix
+++ nix/configs/thelessone/networking/pangolin.nix
@@ -42,6 +42,7 @@
       boot.kernel.sysctl = {
         "net.ipv4.ip_forward" = true;
         "net.ipv6.conf.all.forwarding" = true;
+        "net.ipv6.conf.enp9s0.disable_ipv6" = true;
       };
 
       networking.nat = {
