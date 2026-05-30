4d34caac25c7524a353bad23c37989ac59337884
nanoyaki <hanakretzer@nanoyaki.space>
2026-05-30 23:54:18 +0200

    fix(minecraft): backup path

--- nix/configs/thelessone/servers/games/minecraft/backups.nix
+++ nix/configs/thelessone/servers/games/minecraft/backups.nix
@@ -6,7 +6,7 @@
     }:
 
     {
-      thelessone.backups.nix-minecraft.paths = [ "${config.services.minecraft-servers.dataDir}/*" ];
+      thelessone.backups.nix-minecraft.paths = [ config.services.minecraft-servers.dataDir ];
       thelessone.backups.nix-minecraft.exclude = [
         "${config.services.minecraft-servers.dataDir}/*/world/datapacks/**"
         "${config.services.minecraft-servers.dataDir}/**/*.bak"
